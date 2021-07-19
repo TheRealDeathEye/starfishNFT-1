5aa3f3cc97e249f6a1f13475dc8757f600bd2312
Death Eye <therealdeatheye@gmail.com>
2021-07-19 20:52:11 +0000

    home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './css/App.css';
+import Home from './home';
+import {Route,Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router exact path='/' component={Home} />
     </div>
   );
 }
