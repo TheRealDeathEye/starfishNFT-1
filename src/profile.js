597b3aa8d7f852deddf2d015c717066e8c0db92c
Death Eye <therealdeatheye@gmail.com>
2021-07-20 18:03:45 +0000

    imgs updated

--- src/profile.js
+++ src/profile.js
@@ -5,7 +5,7 @@ function Profile() {
     <div align="center" class="row">
       <h1 style={{paddingTop:'125px',paddingBottom:'20px',textAlign:'center',color:'white'}}>My Profile</h1>
       <div class="col"align="right">
-        <img style={{height:'200px',width:'200px',background:'#313639'}} type="button" class="btn" src="/imgs/logo1.png" alt=""></img>
+        <img style={{height:'200px',width:'200px',background:'#313639'}} type="button" class="btn" src="/imgs/white_logo.png" alt=""></img>
       </div>
       <div class="col"align="left">
         <div class="form-group"style={{width:'25%'}}>
